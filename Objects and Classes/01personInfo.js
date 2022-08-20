2aeefa7e49d1b35e9ccad76fb73a51e170c38ca0
Mihail Donchev <mihaildonchev88@abv.bg>
2022-08-20 13:41:26 +0300

    Homework

---
+++ Objects and Classes/01personInfo.js
@@ -0,0 +1,9 @@
+function personInfo(firstName, secondName, age) {
+
+   let person = {};
+   person.firstName = firstName;
+   person.secondName = secondName;
+   person.age = age
+        console.log(person);
+}
+personInfo("Peter", "Pan", "20");
