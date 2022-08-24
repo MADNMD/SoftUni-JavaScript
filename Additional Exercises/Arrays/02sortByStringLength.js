643614eedb8bd1598a2538aad11e4364352a7725
Mihail Donchev <mihaildonchev88@abv.bg>
2022-08-24 23:00:13 +0300

    .

--- Additional Exercises/Arrays/02sortByStringLength.js
+++
@@ -1,6 +0,0 @@
-function sortByStringLength(array) {
-
-    let sortArray = array.sort((a,b) => a.length - b.length);
-    console.log(sortArray);
-}
-sortByStringLength(["may", "april", "september", "august"]);
