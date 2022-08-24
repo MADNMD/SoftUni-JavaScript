aeee592fb3b565a28f3a8badb0c880a59ff09f83
Mihail Donchev <mihaildonchev88@abv.bg>
2022-08-24 22:57:43 +0300

    .

--- Additional Exercises/Arrays/01filterOutStringsFromAnArray.js
+++
@@ -1,6 +0,0 @@
-function filterOutStringsFromAnArray(array) {
-
-    let filterArray = array.filter(x => typeof x === 'number');
-    console.log(filterArray);
-}
-filterOutStringsFromAnArray([1, 2, "aasf", "1", "123", 123]);
