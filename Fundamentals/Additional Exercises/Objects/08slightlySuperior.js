ee6ef4a589b05f08b88342a004b76f4aaa2760f6
Mihail Donchev <mihaildonchev88@abv.bg>
2022-08-31 12:10:02 +0300

    Additional Exercises

---
+++ Fundamentals/Additional Exercises/Objects/08slightlySuperior.js
@@ -0,0 +1,5 @@
+function slightlySuperior(array1, array2) {
+   console.log(array1 > array2);;
+}
+slightlySuperior(["a", "d", "c"], ["a", "b", "c"]);
+slightlySuperior([true, 10, "zebra"], [true, 10, "zebra"]);
