e53c4df097c417f051a80926127a687828c3487f
Mihail Donchev <mihaildonchev88@abv.bg>
2022-09-01 13:40:59 +0300

    Homework

---
+++ Programming Basic/First Steps In Coding/07projectsCreation.js
@@ -0,0 +1,10 @@
+function projectCreation(input) {
+
+    let name = input[0];
+    let numberProject = Number(input[1]);
+
+    let hours = numberProject * 3;
+
+    console.log(`The architect ${name} will need ${hours} hours to complete ${numberProject} project/s.`);
+}
+projectCreation(["George ","4 "]);
