28f6db0e9091160a0a256539571fa95a3c0e5635
Mihail Donchev <mihaildonchev88@abv.bg>
2022-09-20 14:59:53 +0300

    Homework

---
+++ Fundamentals/Text Processing/12modernTimesOf#(HashTag).js
@@ -0,0 +1,39 @@
+function modernTimesOfHashTag(input) {
+
+    let text = input.split(' ');
+    let result = [];
+    for (let word of text) {
+        if (word.startsWith('#') && word.length > 1) {
+            result.push(word.substring(1));
+        }
+    }
+    let finalResult = [];
+    for (let word of result) {
+        let currentWord = word.split('');
+        let currentWordL = currentWord.length;
+        let flag = true;
+        for (let i = 0; i < currentWordL; i++) {
+            let currentChar = currentWord[i];
+            if(!isNaN(currentChar)){
+                flag = false;
+            }
+        }
+        if (flag) {
+            finalResult.push(word);
+        }
+    }
+    finalResult.forEach(word => console.log(word));
+}
+modernTimesOfHashTag('Nowadays everyone uses # to tag a #special word in #socialMedia');
+
+
+// let text = input.split(' ');
+// let result = [];
+// for (let word of text) {
+//     if (word.startsWith('#')) {
+//         if (word.length > 1) {
+//             word = word.replace('#', ' ').trim().split('');
+//             let wordL = word.length; 
+//         }
+//     }
+// }
