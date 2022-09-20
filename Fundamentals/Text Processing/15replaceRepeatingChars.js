9b10073fc721113f62b20c70e1cfd43903fc28a8
Mihail Donchev <mihaildonchev88@abv.bg>
2022-09-20 20:34:07 +0300

    Homework

---
+++ Fundamentals/Text Processing/15replaceRepeatingChars.js
@@ -0,0 +1,10 @@
+function replaceRepeatingChars(input) {
+    let result = '';
+    for (let letter of input) {
+        if (letter !== result[result.length - 1]) {
+            result += letter;
+        }
+    }
+    console.log(result);
+}
+replaceRepeatingChars('aaaaabbbbbcdddeeeedssaa');
