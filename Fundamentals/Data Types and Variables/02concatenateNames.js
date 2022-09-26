5e829a095a569dbd861c5691af2a3d6dbeb5508b
Mihail Donchev <mihaildonchev88@abv.bg>
2022-09-26 15:53:36 +0300

    Homework
    add solution of problem concatenate names

---
+++ Fundamentals/Data Types and Variables/02concatenateNames.js
@@ -0,0 +1,5 @@
+function concatenateNames(firstName, secondName, delimiter) {
+    let result = firstName + delimiter + secondName;
+    console.log(result);
+}
+concatenateNames('John', 'Smith', '->');
