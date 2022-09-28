01c82b9f52286a3a1b16b79a6b2a4a56b934d213
Mihail Donchev <mihaildonchev88@abv.bg>
2022-09-28 12:12:07 +0300

    Homework
    add solution of problem reversed char

---
+++ Fundamentals/Data Types and Variables/16reversedChars.js
@@ -0,0 +1,15 @@
+function reversedChars(first, two, third) {
+    let result = [];
+    result.push(first, two, third)
+    console.log(result.reverse().join(' '));
+}
+reversedChars('A', 'B', 'C');
+
+// let char = [];
+// let result = []
+// char.push(first,two,third)
+// let charL = char.length;
+// for(let i = charL -1; i >= 0; i--){
+//     result.push(char[i])
+// }
+// console.log(result.join(' '));
