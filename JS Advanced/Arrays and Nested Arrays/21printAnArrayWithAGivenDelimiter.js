74a12d66f01eb203cd8bded471c2948b39578c57
Mihail Donchev <mihaildonchev88@abv.bg>
2022-10-18 16:29:20 +0300

    Homework
    add solution of problem print an array with given delimiter

---
+++ JS Advanced/Arrays and Nested Arrays/21printAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,4 @@
+function printAnArrayWithAGivenDelimiter(array, delimiter) {
+    console.log(array.join(delimiter));
+}
+printAnArrayWithAGivenDelimiter(['One', 'Two', 'Three', 'Four', 'Five'], '-');
