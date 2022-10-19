f128f062eef1a5015929ac930c1bd28e4a93f257
Mihail Donchev <mihaildonchev88@abv.bg>
2022-10-19 23:34:06 +0300

    Homework
    add solution of problem city record

---
+++ JS Advanced/Objects and Composition/01cityRecord.js
@@ -0,0 +1,12 @@
+function cityRecord(city, population, treasury){
+
+    const cityRecord = {
+        name: city,
+        population: population,
+        treasury: treasury,
+    }
+    return cityRecord;
+}
+cityRecord('Tortuga',
+7000,
+15000);
