452b4ed7a477bcde3c028c3e83a84bff83c8e03f
Mihail Donchev <mihaildonchev88@abv.bg>
2022-10-20 13:16:50 +0300

    Homework
    add solution of problem city taxes

---
+++ JS Advanced/Objects and Composition/03cityTaxes.js
@@ -0,0 +1,26 @@
+function cityTaxes(city, population, treasury) {
+
+    const cityRecord = {
+        name: city,
+        population: population,
+        treasury: treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage) {
+            percentage /= 100;
+            this.population *= percentage + 1;
+        },
+        applyRecession(percentage) {
+            percentage /= 100;
+            this.treasury *= 1 - percentage;
+        }
+    }
+    return cityRecord;
+}
+const city = cityTaxes('Tortuga', 7000, 15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
