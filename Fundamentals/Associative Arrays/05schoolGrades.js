cfda6421e6704e2444cf79fcce9ceb2ae972bde1
Mihail Donchev <mihaildonchev88@abv.bg>
2022-11-03 16:23:08 +0200

    Update 05schoolGrades.js

--- Fundamentals/Associative Arrays/05schoolGrades.js
+++ Fundamentals/Associative Arrays/05schoolGrades.js
@@ -1,50 +1,31 @@
 function schoolGrades(input) {
 
-    let schoolDiary = {};
-    for (let info of input) {
-        let [name, ...grades] = info.split(' ');
-        if (!schoolDiary.hasOwnProperty(name)) {
-            schoolDiary[name] = [];
+    const studentsList = {};
+    const schoolDiary = input;
+
+    schoolDiary.forEach(line => {
+        let [name, ...grades] = line.split(' ');
+
+        if (!studentsList[name]) {
+            studentsList[name] = grades;
+        } else {
+            grades.forEach(grade => {
+                studentsList[name].push(grade);
+            });
         }
-        let existsGrade = schoolDiary[name];
-        for (let grade of grades) {
-            existsGrade.push(Number(grade));
-        }
-    }
-    let key = Object.keys(schoolDiary);
-    let sortedName = key.sort((a, b) => a.localeCompare(b));
-    for (let name of sortedName) {
-        let average = schoolDiary[name].reduce((a, b) => a + b, 0) / schoolDiary[name].length;
-        console.log(`${name}: ${average.toFixed(2)}`);
-        //console.log(`${name}: ${schoolDiary[name]}`);
-    }
+    });
+    const sortedByName = Object.keys(studentsList);
+    sortedByName.sort((a, b) => a.localeCompare(b));
+
+    sortedByName.forEach(student => {
+        const avvGrade = studentsList[student]
+            .map(Number)
+            .reduce((a, b) => a + b, 0) / studentsList[student].length;
+        console.log(`${student}: ${avvGrade.toFixed(2)}`);
+    });
 }
 schoolGrades([
     'Lilly 4 6 6 5',
     'Tim 5 6',
     'Tammy 2 4 3',
     'Tim 6 6']);
-
-
-    // let schoolBook = new Map();
-    // for (let line of input) {
-    //     let tokens = line.split(' ');
-    //     let name = tokens.shift();
-    //     let grades = tokens.map(Number);
-    //     if (!schoolBook.has(name)) {
-    //         schoolBook.set(name, []);
-    //     }
-    //     let existsGrade = schoolBook.get(name);
-    //     for (let grade of grades) {
-    //         existsGrade.push(Number(grade));
-    //     }
-    // }
-    // let sortedName = Array.from(schoolBook).sort((a, b) => a[0].localeCompare(b[0]));
-    // for (let [name, grades] of sortedName) {
-    //     let averageGrade = 0;
-    //     for (let grade of grades) {
-    //         averageGrade += grade;
-    //     }
-    //     averageGrade /= grades.length;
-    //     console.log(`${name}: ${averageGrade.toFixed(2)}`);
-    // }
