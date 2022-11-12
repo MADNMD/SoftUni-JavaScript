ff9612b381581b26ff5a44fe50bf2cea82c90de4
Mihail Donchev <mihaildonchev88@abv.bg>
2022-11-12 13:15:12 +0200

    Update 19passwordGenerator.js

--- Fundamentals/Text Processing/19passwordGenerator.js
+++ Fundamentals/Text Processing/19passwordGenerator.js
@@ -23,36 +23,3 @@ function passwordGenerator(input) {
     console.log(`Your generated password is ${password}`);
 }
 passwordGenerator(['ilovepizza', 'ihatevegetables', 'orange']);
-
-
-//------------------------------------------------------
-
-    // let firstStr = input[0];
-    // let secondStr = input[1];
-    // let word = input[2];
-
-    // let splitWord = word.split('');
-    // let splitWordL = splitWord.length;
-
-    // let allStr = firstStr + secondStr;
-    // allStr = allStr.toLowerCase();
-    // let arrayAllStr = allStr.split('');
-    // let arrayAllStrL = arrayAllStr.length;
-    // let vowelIndex = 0;
-    // let reversePassword = [];
-
-    // for (let i = 0; i < arrayAllStrL; i++) {
-    //     let currentChar = arrayAllStr[i];
-    //     if (currentChar === String.fromCharCode(97) || currentChar === String.fromCharCode(101) ||
-    //         currentChar === String.fromCharCode(105) || currentChar === String.fromCharCode(111) ||
-    //         currentChar === String.fromCharCode(117)) {
-    //         let indexOfChar = vowelIndex % splitWordL;
-    //         vowelIndex++;
-    //         let currentChar = splitWord[indexOfChar];
-    //         reversePassword.push(currentChar.toUpperCase());
-    //     }else {
-    //         reversePassword.push(arrayAllStr[i]);
-    //     }
-    // }
-    // let result = reversePassword.reverse().join('');
-    // console.log(`Your generated password is ${result}`);
