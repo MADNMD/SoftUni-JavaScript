00d923ec4dce50d0b2d0a1c8fbd4f5a43549e593
Mihail Donchev <mihaildonchev88@abv.bg>
2022-11-12 13:14:13 +0200

    Update 13extractFile.js

--- Fundamentals/Text Processing/13extractFile.js
+++ Fundamentals/Text Processing/13extractFile.js
@@ -8,18 +8,3 @@ function extractFile(input) {
     console.log(`File extension: ${extension}`);
 }
 extractFile('C:\\Internal\\training-internal\\Template.pptx');
-
-//--------------------------------------
-
-
-    // let file = input.split('\\');
-    // let nameOfFile = file.pop().split('.');
-    // let extension = nameOfFile.pop();
-    // let template = nameOfFile.join('.');
-    // let obj = {
-    //     'File name': template,
-    //     'File extension': extension,
-    // }
-    // for (let name in obj) {
-    //     console.log(`${name}: ${obj[name]}`);
-    // }
