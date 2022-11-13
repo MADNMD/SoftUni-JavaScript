4dc8f6872d81f1e759a1a124eb975585dd29550b
Mihail Donchev <mihaildonchev88@abv.bg>
2022-11-13 12:23:38 +0200

    Update 16pascalCaseSplitter.js

--- Fundamentals/Text Processing/16pascalCaseSplitter.js
+++ Fundamentals/Text Processing/16pascalCaseSplitter.js
@@ -10,26 +10,3 @@ function pascalCaseSplitter(input) {
     console.log(words.join(', '));
 }
 pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
-
-//---------------------------------
-
-// let sentence = input.split(/(?=[A-Z])/);
-// console.log(sentence.join(', '));
-
-
-//-----------------------------------------------------
-
-
-    // let words = [];
-    // let currentWord = input[0];
-    // let text = input.length;
-    // for(let i = 1; i < text; i++){
-    //     if(input[i].toUpperCase() !== input[i]){
-    //         currentWord = currentWord.concat(input[i]);
-    //     }else{
-    //         words.push(currentWord);
-    //         currentWord = input[i];
-    //     }
-    // }
-    // words.push(currentWord);
-    // console.log(words.join(', '));
