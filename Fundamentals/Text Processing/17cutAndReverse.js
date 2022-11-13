b7f3c2809012b1ac0f4119baab50c3641d6ef35a
Mihail Donchev <mihaildonchev88@abv.bg>
2022-11-13 12:24:38 +0200

    Update 17cutAndReverse.js

--- Fundamentals/Text Processing/17cutAndReverse.js
+++ Fundamentals/Text Processing/17cutAndReverse.js
@@ -14,22 +14,3 @@ function cutAndReverse(input) {
     console.log(secondHalf);
 }
 cutAndReverse('sihToDtnaCuoYteBIboJsihTtAdooGoSmI');
-
-
-//--------------------------------------------------------
-
-
-    // let firstHalf = [];
-    // let secondHalf = [];
-    // let text = input.split('');
-    // let textL = text.length;
-    // for (let i = 0; i < textL; i++) {
-    //     let currentChar = text[i];
-    //     if (firstHalf.length < text.length / 2) {
-    //         firstHalf.push(currentChar);
-    //     } else {
-    //         secondHalf.push(currentChar);
-    //     }
-    // }
-    // console.log(firstHalf.reverse().join(''));
-    // console.log(secondHalf.reverse().join(''));
