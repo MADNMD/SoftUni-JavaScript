e3082049f4c2aba24b033d30f19275eb44d64cca
Mihail Donchev <mihaildonchev88@abv.bg>
2022-11-25 13:55:53 +0200

    Homework
    add JS file

---
+++ JS Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -0,0 +1,20 @@
+function solve() {
+
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let search = document.getElementById('searchField');
+   const studentsInfo = Array.from(document.querySelectorAll('tbody tr'));
+
+   function onClick() {
+
+      for (let line of studentsInfo) {
+         line.classList.remove('select');
+         if (search.value === '') {
+            line.classList.remove('select');
+         }
+         if (search.value !== '' && line.textContent.includes(search.value)) {
+            line.className = 'select';
+         }
+      }
+      search.value = '';
+   }
+}
