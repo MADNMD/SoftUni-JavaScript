7e9ae44bf535167a06b67df508759e34d1a0cded
Mihail Donchev <mihaildonchev88@abv.bg>
2022-12-05 14:26:39 +0200

    Homework
    add JS file

---
+++ JS Advanced/DOM Manipulations and Events - Lab/03. Add-Delete/app.js
@@ -0,0 +1,24 @@
+function addItem() {
+
+    const input = document.getElementById('newItemText');
+    const ulElement = document.getElementById('items');
+
+    const newLiElement = document.createElement('li');
+
+    newLiElement.textContent = input.value
+
+    ulElement.appendChild(newLiElement);
+
+    const deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    newLiElement.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', onDelete);
+
+    function onDelete(event) {
+        event.target.parentElement.remove();
+    }
+
+    input.value = '';
+}
