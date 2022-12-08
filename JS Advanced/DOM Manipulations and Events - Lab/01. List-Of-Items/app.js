412bf2fda83cea8ba887af55538136b85d8aa7a6
Mihail Donchev <mihaildonchev88@abv.bg>
2022-12-08 17:33:02 +0200

    Update app.js

--- JS Advanced/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
@@ -12,43 +12,4 @@ function addItem() {
         itemsElements.appendChild(newLiElement);
     }
     inputElement.value = '';
-}  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function addItem() {
-
-//     const inputElement = document.getElementById('newItemText');
-//     const itemsElements = document.getElementById('items');
-
-//     const newLiElement = document.createElement('li');
-
-//     newLiElement.textContent = inputElement.value;
-
-//     itemsElements.appendChild(newLiElement);
-//     inputElement.value = '';
-// }   
+}   
