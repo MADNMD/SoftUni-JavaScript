bf8be7de2b71704f02e96d2674f97d2043626f54
Mihail Donchev <mihaildonchev88@abv.bg>
2022-12-18 15:17:33 +0200

    Homework

---
+++ JS Advanced/Advanced Functions - Lab/06listProcessor2.js
@@ -0,0 +1,17 @@
+function listProcessor(input) {
+
+    const inputElements = input.slice();
+    let words = [];
+
+    let result = {
+        add: (string) => words.push(string),
+        remove: (string) => words = words.filter((word) => word !== string),
+        print: () => console.log(words.join(','))
+    };
+
+    inputElements.forEach(element => {
+        let [command, value] = element.split(' ');
+        result[command](value);
+    });
+}
+listProcessor(['add hello', 'add again', 'remove hello', 'add again', 'print']);
