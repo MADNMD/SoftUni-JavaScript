a51c398a9f857c56191b6371b270e252ecab81c9
Mihail Donchev <mihaildonchev88@abv.bg>
2023-01-13 12:38:52 +0200

    Update 06biggerHalf.js

--- JS Advanced/Arrays and Nested Arrays/06biggerHalf.js
+++ JS Advanced/Arrays and Nested Arrays/06biggerHalf.js
@@ -8,12 +8,3 @@ return secondHalf;
 }
 biggerHalf([4, 7, 2, 5]);
 biggerHalf([3, 19, 14, 7, 2, 19, 6]);
-
-    // let sortedArray = array.sort((a, b) => a - b);
-
-    // const secondHalf = [];
-
-    // for (let i = Math.floor(sortedArray.length / 2); i < sortedArray.length; i++) {
-    //     secondHalf.push(sortedArray[i]);
-    // }
-    // return secondHalf;
