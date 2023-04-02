62ff25b4067439e15303796ca4e36ace6c62f630
Mihail Donchev <mihaildonchev88@abv.bg>
2023-04-02 16:41:07 +0300

    Create auth.js

---
+++ JS Applications/07.Routing Exercise/01.Furniture/src/auth/auth.js
@@ -0,0 +1,35 @@
+import { get, post } from "../api/api.js";
+import { clearUserData, setUserData } from "../util.js";
+
+export async function login(email, password) {
+
+    const result = await post('/users/login', { email, password });
+
+    const userData = {
+        email: result.email,
+        id: result._id,
+        token: result.accessToken,
+    }
+
+    setUserData(userData);
+    return result;
+}
+
+export async function register(email, password) {
+
+    const result = await post('/users/register', { email, password });
+
+    const userData = {
+        email: result.email,
+        id: result._id,
+        token: result.accessToken,
+    }
+
+    setUserData(userData);
+    return result;
+}
+
+export async function logout() {
+    await get('/users/logout');
+    clearUserData();
+}
