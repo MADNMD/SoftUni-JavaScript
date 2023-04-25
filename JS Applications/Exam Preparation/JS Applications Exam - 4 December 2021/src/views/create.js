867a92532f4bd5210dc7e89aad4abfe0f987fe19
Mihail Donchev <mihaildonchev88@abv.bg>
2023-04-25 20:01:34 +0300

    Create create.js

---
+++ JS Applications/Exam Preparation/JS Applications Exam - 4 December 2021/src/views/create.js
@@ -0,0 +1,63 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { createAlbum } from "../api/data.js";
+
+const createTemplate = (onCreate) => html`
+<section class="createPage">
+    <form @submit=${onCreate}>
+        <fieldset>
+            <legend>Add Album</legend>
+
+            <div class="container">
+                <label for="name" class="vhide">Album name</label>
+                <input id="name" name="name" class="name" type="text" placeholder="Album name">
+
+                <label for="imgUrl" class="vhide">Image Url</label>
+                <input id="imgUrl" name="imgUrl" class="imgUrl" type="text" placeholder="Image Url">
+
+                <label for="price" class="vhide">Price</label>
+                <input id="price" name="price" class="price" type="text" placeholder="Price">
+
+                <label for="releaseDate" class="vhide">Release date</label>
+                <input id="releaseDate" name="releaseDate" class="releaseDate" type="text" placeholder="Release date">
+
+                <label for="artist" class="vhide">Artist</label>
+                <input id="artist" name="artist" class="artist" type="text" placeholder="Artist">
+
+                <label for="genre" class="vhide">Genre</label>
+                <input id="genre" name="genre" class="genre" type="text" placeholder="Genre">
+
+                <label for="description" class="vhide">Description</label>
+                <textarea name="description" class="description" placeholder="Description"></textarea>
+
+                <button class="add-album" type="submit">Add New Album</button>
+            </div>
+        </fieldset>
+    </form>
+</section>`;
+
+export async function createPage(ctx) {
+
+    ctx.render(createTemplate(onCreate));
+
+    async function onCreate(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        const albumData = {
+            name: formData.get('name').trim(),
+            imgUrl: formData.get('imgUrl').trim(),
+            price: formData.get('price').trim(),
+            releaseDate: formData.get('releaseDate').trim(),
+            artist: formData.get('artist').trim(),
+            genre: formData.get('genre').trim(),
+            description: formData.get('description').trim(),
+        }
+
+        if (Object.values(albumData).some(field => field === '')) {
+            return alert('All fiedls are required');
+        }
+        await createAlbum(albumData);
+        ctx.page.redirect('/catalog');
+    }
+}
