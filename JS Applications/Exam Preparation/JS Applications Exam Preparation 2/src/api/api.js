93e0b4736482e87c7298e011d32a552a1fb9278d
Mihail Donchev <mihaildonchev88@abv.bg>
2023-05-10 17:17:09 +0300

    Create api.js

---
+++ JS Applications/Exam Preparation/JS Applications Exam Preparation 2/src/api/api.js
@@ -0,0 +1,62 @@
+import { getUserData } from "../util.js";
+
+const hostname = 'http://localhost:3030';
+
+async function request(url, option) {
+
+    try {
+
+        const response = await fetch(hostname + url, option);
+
+        if (response.ok !== true) {
+            const err = await response.json();
+            throw new Error(err.message);
+        }
+
+        if (response.status === 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+
+    } catch (error) {
+        alert(error.message);
+        throw error;
+    }
+}
+
+function createOption(method = 'get', data) {
+
+    const option = {
+        method,
+        headers: {}
+    };
+
+    if (data !== undefined) {
+        option.headers['Content-Type'] = 'application/json';
+        option.body = JSON.stringify(data);
+    }
+
+    const userData = getUserData();
+
+    if (userData) {
+        option.headers['X-Authorization'] = userData.token;
+    }
+    return option;
+}
+
+export async function get(url) {
+    return request(url, createOption());
+}
+
+export async function post(url, data) {
+    return request(url, createOption('post', data));
+}
+
+export async function put(url, data) {
+    return request(url, createOption('put', data));
+}
+
+export async function del(url) {
+    return request(url, createOption('delete'));
+}
