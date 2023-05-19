ee2f61166c323fe82267f5e9c410b289af743ebe
Mihail Donchev <mihaildonchev88@abv.bg>
2023-05-19 20:38:37 +0300

    Create register.js

---
+++ JS Applications/Exam Preparation/JS Applications Retake Exam - 13 Dec 2022/Cosmetic Kingdom_Resources/src/views/register.js
@@ -0,0 +1,44 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { navigationView } from "../app.js";
+import { register } from "../auth/auth.js";
+
+const registerTemplate = (onRegister) => html`
+<section id="register">
+    <div class="form">
+        <h2>Register</h2>
+        <form @submit=${onRegister} class="register-form">
+            <input type="text" name="email" id="register-email" placeholder="email" />
+            <input type="password" name="password" id="register-password" placeholder="password" />
+            <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
+            <button type="submit">register</button>
+            <p class="message">Already registered? <a href="/login">Login</a></p>
+        </form>
+    </div>
+</section>`;
+
+export async function registerPage(ctx){
+
+    ctx.render(registerTemplate(onRegister));
+
+    async function onRegister(event){
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+        const rePassword = formData.get('re-password').trim();
+
+        if(email === '' || password === '' || rePassword === ''){
+            return alert('All fields are required');
+        }
+
+        if(password !== rePassword){
+            return alert('Incorrect password');
+        }
+        await register(email, password);
+        event.target.reset();
+        navigationView();
+        ctx.page.redirect('/catalog');
+    }
+}
