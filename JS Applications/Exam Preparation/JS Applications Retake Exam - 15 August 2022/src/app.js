2ecbc76b773ba41da63323746af410f9fe4bbe8e
Mihail Donchev <mihaildonchev88@abv.bg>
2023-05-22 18:07:48 +0300

    Create app.js

---
+++ JS Applications/Exam Preparation/JS Applications Retake Exam - 15 August 2022/src/app.js
@@ -0,0 +1,54 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import page from '../node_modules/page/page.mjs';
+import { logout } from './api/api.js';
+import { getUserData } from './util.js';
+import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { searchPage } from './views/search.js';
+
+const root = document.getElementById('container');
+
+function decorationContex(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.navigationView = navigationView;
+
+    next();
+}
+
+export function navigationView() {
+
+    const userData = getUserData();
+
+    if (userData) {
+        document.querySelector('.user').style.display = 'inline';
+        document.querySelector('.guest').style.display = 'none';
+    } else {
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = 'inline';
+    }
+}
+
+document.getElementById('logoutBtn').addEventListener('click', async (event) => {
+    event.preventDefault();
+    logout();
+    navigationView();
+    page.redirect('/catalog');
+});
+
+page(decorationContex);
+navigationView();
+page.redirect('/home');
+page('/home', homePage);
+page('/catalog', catalogPage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/search', searchPage);
+page.start();
