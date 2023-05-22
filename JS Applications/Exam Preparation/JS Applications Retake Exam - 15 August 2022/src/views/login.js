a3b4f89bc70c8cc3c1de21281f55173e090d2d72
Mihail Donchev <mihaildonchev88@abv.bg>
2023-05-22 18:06:21 +0300

    Create login.js

---
+++ JS Applications/Exam Preparation/JS Applications Retake Exam - 15 August 2022/src/views/login.js
@@ -0,0 +1,38 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { login } from '../api/api.js';
+
+const loginTempleate = (onLogin) => html`
+<section id="login">
+    <div class="form">
+        <h2>Login</h2>
+        <form @submit=${onLogin} class="login-form">
+            <input type="text" name="email" id="email" placeholder="email" />
+            <input type="password" name="password" id="password" placeholder="password" />
+            <button type="submit">login</button>
+            <p class="message">
+                Not registered? <a href="/register">Create an account</a>
+            </p>
+        </form>
+    </div>
+</section>`;
+
+export async function loginPage(ctx) {
+    ctx.render(loginTempleate(onLogin));
+
+    async function onLogin(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        if (email === '' || password === '') {
+            return alert('All fields are required');
+        }
+        await login(email, password);
+        event.target.reset();
+        ctx.navigationView();
+        ctx.page.redirect('/catalog');
+    }
+}
