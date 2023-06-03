69977f4e7c20b085d4dbc79a71ac0be1a865d05b
Mihail Donchev <mihaildonchev88@abv.bg>
2023-06-03 14:53:39 +0300

    Create navigation.js

---
+++ JS Applications/Exam Preparation/JS Applications Retake Exam- 16 December 2021/src/layout/navigation.js
@@ -0,0 +1,20 @@
+import { getUserData } from "../util/util.js";
+
+export function navigationViews() {
+
+    const userData = getUserData();
+
+    if (userData) {
+        document.querySelectorAll('.user')[0].style.display = 'inline';
+        document.querySelectorAll('.user')[1].style.display = 'inline';
+        document.querySelectorAll('.user')[2].style.display = 'inline';
+        document.querySelectorAll('.guest')[0].style.display = 'none';
+        document.querySelectorAll('.guest')[1].style.display = 'none';
+    } else {
+        document.querySelectorAll('.user')[0].style.display = 'none';
+        document.querySelectorAll('.user')[1].style.display = 'none';
+        document.querySelectorAll('.user')[2].style.display = 'none';
+        document.querySelectorAll('.guest')[0].style.display = 'inline';
+        document.querySelectorAll('.guest')[1].style.display = 'inline';
+    }
+}
