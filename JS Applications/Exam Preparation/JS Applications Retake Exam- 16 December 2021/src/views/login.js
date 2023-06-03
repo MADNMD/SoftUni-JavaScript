e07b39015525e2594d8adccb2c78c15dd4f561b3
Mihail Donchev <mihaildonchev88@abv.bg>
2023-06-03 14:57:19 +0300

    Create login.js

---
+++ JS Applications/Exam Preparation/JS Applications Retake Exam- 16 December 2021/src/views/login.js
@@ -0,0 +1,47 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { login } from "../auth/auth.js";
+import { navigationViews } from "../layout/navigation.js";
+
+const loginTemplate = (onLogin) => html`
+<section id="loginaPage">
+    <form @submit=${onLogin} class="loginForm">
+        <h2>Login</h2>
+        <div>
+            <label for="email">Email:</label>
+            <input id="email" name="email" type="text" placeholder="steven@abv.bg" value="">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input id="password" name="password" type="password" placeholder="********" value="">
+        </div>
+
+        <button class="btn" type="submit">Login</button>
+
+        <p class="field">
+            <span>If you don't have profile click <a href="/register">here</a></span>
+        </p>
+    </form>
+</section>`;
+
+export async function loginPage(ctx){
+
+    ctx.render(loginTemplate(onLogin));
+
+    async function onLogin(event){
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        if(email === '' || password === ''){
+            return alert('All fields are required');
+        }
+        
+        await login(email, password);
+        event.target.reset();
+        navigationViews();
+        ctx.page.redirect('/home');
+    }
+}
