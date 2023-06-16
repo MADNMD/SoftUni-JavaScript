c452a281073882bf22c61968e856c193cc1e4e2f
Mihail Donchev <mihaildonchev88@abv.bg>
2023-06-16 23:41:04 +0300

    Create database.js

---
+++ JS Back-End/Exam Preparation/JS Back-End Exam - 01 November 2020/Tutorials/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const { DB_CONECTION_STRING } = require('./env')
+
+exports.initDB = () => {
+
+    mongoose.connection.on('open', () => console.log('Database is conected!'));
+
+    return mongoose.connect(DB_CONECTION_STRING);
+
+}
