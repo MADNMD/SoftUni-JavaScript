7db533cbe9ae1daac7a2b7714d7202860fae9005
Mihail Donchev <mihaildonchev88@abv.bg>
2023-06-25 23:14:37 +0300

    Create homeController.js

---
+++ JS Back-End/05.Workshop Session and Authentication/Cubicle project Part 3/src/controllers/homeController.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const cubeServices = require('../services/cubeServices');
+
+router.get('/', async (req, res) => {
+
+    let { search, from, to } = req.query;
+
+    const cubes = await cubeServices.getAll(search, Number(from), Number(to));
+
+    res.render('index', { cubes });
+
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+module.exports = router;
