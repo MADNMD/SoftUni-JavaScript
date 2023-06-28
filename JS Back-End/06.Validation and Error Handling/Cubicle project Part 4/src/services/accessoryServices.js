294902ff52f86733cc585de565900c94656e4912
Mihail Donchev <mihaildonchev88@abv.bg>
2023-06-28 15:17:26 +0300

    Create accessoryServices.js

---
+++ JS Back-End/06.Validation and Error Handling/Cubicle project Part 4/src/services/accessoryServices.js
@@ -0,0 +1,7 @@
+const Accessory = require('../models/Accessory');
+
+exports.getAll = () => Accessory.find();
+
+exports.getAllWithout = (ids) => Accessory.find({ _id: { $nin: ids } });
+
+exports.create = (accessoryData) => Accessory.create(accessoryData); 
