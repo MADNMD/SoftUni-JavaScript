e71f7e0adbf0cfd04416b5b29047891d3cb09bf4
Mihail Donchev <mihaildonchev88@abv.bg>
2023-06-28 15:17:11 +0300

    Create authServices.js

---
+++ JS Back-End/06.Validation and Error Handling/Cubicle project Part 4/src/services/authServices.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+const User = require('../models/User');
+const { saltRounds, secret } = require('../constants');
+
+// const saltRounds = 10;
+// const secret = 'djakjjfldsnlksjfabdhasgdywgheban';
+
+exports.register = async ({ username, password, repeatPassword }) => {
+
+    if (password != repeatPassword) {
+        throw new Error('Repeat password should match password');
+        // return false;
+    };
+
+    let hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    let createdUser = User.create({
+        username,
+        password: hashedPassword,
+    });
+   
+    return createdUser;
+}
+
+exports.login = async ({ username, password }) => {
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw{
+            message: 'Invalid username or password',
+        }
+        // return;
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw {
+            message: 'Invalid username or password',
+        }
+    }
+
+    // const jwtPromiseSign = promisify(jwt.sign);
+    // jwtPromiseSign()
+
+    let result = new Promise((resolve, reject) => {
+        // expiresIn колко време да е валиден tokenа
+        jwt.sign({ _id: user._id, username: user.username }, secret, { expiresIn: '2d' }, (err, token) => {
+
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(token);
+        });
+    });
+    return result;
+}
