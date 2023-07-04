24983d6b32bc23f1df6d929a2f523ff98d2a0040
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-04 21:56:36 +0300

    Create authController.js

---
+++ JS Back-End/Exam Preparation/Exam Praparation Petstagram/Petstagram/src/controllers/authController.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+
+const authServices = require('../services/authServices');
+const { getErrorMessage } = require('../utils/errorHellper');
+const { isAuth, isGuest } = require('../middlewares/authMiddleware');
+const { COOKIE_SESION_NAME } = require('../constants')
+
+router.get('/login', isGuest, (req, res) => {
+    res.render('auth/login', { pageTitle: 'Login Page' });
+});
+
+router.post('/login', isGuest, async (req, res) => {
+
+    const { username, password } = req.body;
+
+    try {
+
+        const user = await authServices.login(username, password);
+        const token = await authServices.createToken(user);
+
+        res.cookie(COOKIE_SESION_NAME, token, { httpOnly: true });
+        res.redirect('/');
+    } catch (error) {
+        return res.render('auth/login', { ...req.body, error: getErrorMessage(error) });
+    }
+});
+
+router.get('/register', isGuest, (req, res) => {
+    res.render('auth/register', { pageTitle: 'Register Page' });
+});
+
+router.post('/register', isGuest, async (req, res) => {
+
+    const { username, email, password, rePassword } = req.body;
+
+    if (password !== rePassword) {
+        return res.render('auth/register', { ...req.body, error: 'Password missmatch' })
+    }
+
+    try {
+
+        const createdUser = await authServices.create({ username, email, password, rePassword });
+        const token = await authServices.createToken(createdUser);
+
+        res.cookie(COOKIE_SESION_NAME, token, { httpOnly: true });
+        res.redirect('/');
+    } catch (error) {
+        return res.render('auth/register', { ...req.body, error: getErrorMessage(error) });
+    }
+});
+
+router.get('/logout', isAuth, (req, res) => {
+    res.clearCookie(COOKIE_SESION_NAME);
+    res.redirect('/');
+});
+
+module.exports = router;
