5b624dfddc675b821147579f4791febf26ea0dbc
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-05 18:10:12 +0300

    Create routes.js

---
+++ JS Back-End/Exam Preparation/JS Back-End Exam - 19 April 2022/Action House/routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
+const auctionController = require('./controllers/auctionController');
+
+router.use(homeController);
+router.use('/auth', authController);
+router.use('/auction', auctionController);
+router.use('*', (req, res) => {
+    res.render('404', { sitePage: '404 Page' })
+});
+
+
+module.exports = router;
