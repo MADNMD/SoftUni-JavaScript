d8eba7cab3002e3ffad478e9bea7a2cf55959506
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-09 20:10:18 +0300

    Create index.js

---
+++ JS Back-End/Exam Preparation/JS Back-End Exam - 25 Jun 2022/Crypto-Trade/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+
+const { initDB } = require('./config/database');
+const { PORT } = require('./config/env');
+const router = require('./routes');
+const { errorHanlder } = require('./middlewares/errorHandlerMiddleware');
+const { auth } = require('./middlewares/authMiddleware')
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(auth);
+app.use(router);
+app.use(errorHanlder);
+
+initDB();
+
+app.listen(PORT, () => console.log(`App is listenig at port ${PORT}`));
