cfee43cecf2136ff42f6131c7c79079c4ebec3a2
Mihail Donchev <mihaildonchev88@abv.bg>
2023-08-15 18:15:37 +0300

    Create expressConfig.js

---
+++ JS Back-End/Exam Preparation/JS Back-End Exam - 27 June 2021/Shared Trip/src/config/expressConfig.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+function expressConfig(app) {
+
+    app.use(express.static('src/public'));
+    app.use(express.urlencoded({ extended: false }));
+}
+
+module.exports = expressConfig;
