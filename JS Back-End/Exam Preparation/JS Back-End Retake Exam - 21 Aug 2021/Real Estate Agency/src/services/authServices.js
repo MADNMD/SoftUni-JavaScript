fbffdb832408d53a3f14d39077146031b9b324c1
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-02 19:27:52 +0300

    Create authServices.js

---
+++ JS Back-End/Exam Preparation/JS Back-End Retake Exam - 21 Aug 2021/Real Estate Agency/src/services/authServices.js
@@ -0,0 +1,47 @@
+const User = require('../models/User');
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const { SECRET } = require('../config/evn');
+
+exports.create = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+
+        throw {
+            message: 'Invalid email or password'
+        };
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    // console.log(password)
+    // console.log(user.password)
+    if (!isValid) {
+
+        throw {
+            message: 'Invalid email or password'
+        }
+    }
+    return user;
+}
+
+exports.createToken = (user) => {
+
+    const payload = { _id: user._id, name: user.name, username: user.username };
+    const option = { expiresIn: '2d' };
+
+    const tokenPromise = new Promise((resolve, reject) => {
+        jwt.sign(payload, SECRET, option, (err, decodedToken) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(decodedToken)
+        });
+    });
+    return tokenPromise;
+}
